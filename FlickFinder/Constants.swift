44f27b1a42bb3ce7fa4cd63e5d0014eaa9bde8c7
Sagar Choudhary <sagar.choudhary96@gmail.com>
2018-11-14 19:15:41 +0530

    Add: generate URL based on user search

---
+++ FlickFinder/Constants.swift
@@ -0,0 +1,58 @@
+//
+//  Constants.swift
+//  FlickFinder
+//
+//  Created by Sagar Choudhary on 14/11/18.
+//  Copyright © 2018 Sagar Choudhary. All rights reserved.
+//
+
+struct Constants {
+    struct Flickr {
+        static let APIScheme = "https"
+        static let APIHost = "api.flickr.com"
+        static let APIBaseURL = "/services/rest"
+        
+        static let SearchBoxWidth = 1.0
+        static let SearchBoxHeight = 1.0
+        static let SearchLatRange = (-90.0, 90.0)
+        static let SearchLongRange = (-180.0, 180.0)
+    }
+    
+    struct FlickrParameterKeys {
+        static let Method = "method"
+        static let APIKey = "api_key"
+        static let Extras = "extras"
+        static let Format = "format"
+        static let NoJSONCallback = "nojsoncallback"
+        static let SafeSearch = "safe_search"
+        static let Text = "text"
+        static let BoundingBox = "bbox"
+        static let page = "page"
+    }
+    struct FlickrParameterValues {
+        static let PhotosSearchMethod = "flickr.photos.search"
+        static let APIKey = "FLICKR_API_KEY_HERE"
+        static let MediumURL = "url_m"
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "nojsoncallback"
+        static let SafeSearch = "1"
+    }
+    
+    struct FlickrResponseKeys {
+        static let Photos = "photos"
+        static let Photo = "photo"
+        static let Title = "title"
+        static let ImageUrl = "url_m"
+        static let Status = "status"
+    }
+}
+func getParamPair() -> Dictionary<String, Any> {
+    return [
+        Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.PhotosSearchMethod,
+        Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+        Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+        Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+        Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback,
+        Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.SafeSearch
+    ]
+}
--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -49,30 +49,72 @@ class ViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    
+    //MARK: Search by text phrase
     @IBAction func textFieldSearch(_ sender: Any) {
         userDidTapView(self)
-    }
-    
-    @IBAction func locationSearch(_ sender: Any) {
-        userDidTapView(self)
-    }
-    
-    private func resignIfFirstResponsder(textfield: UITextField) {
-        if (textfield.isFirstResponder) {
-            textfield.resignFirstResponder()
+        enableUI(enabled: false)
+        
+        if !textSearchField.text!.isEmpty {
+            photoLabel.text = "Searching . . ."
+            var parameters: [String: String?] = getParamPair() as! [String : String]
+            parameters[Constants.FlickrParameterKeys.Text] = textSearchField.text
+            displayImageFromFlickrBySearch(parameters: parameters as [String : AnyObject])
+        } else {
+            enableUI(enabled: true)
+            photoLabel.text = "Text Phrase is Empty"
         }
     }
     
-    private func userDidTapView(_ sender: AnyObject) {
-        resignIfFirstResponsder(textfield: textSearchField)
-        resignIfFirstResponsder(textfield: longitudeTextField)
-        resignIfFirstResponsder(textfield: latitudeTextField)
+    //MARK: Search by the location coordinate
+    @IBAction func locationSearch(_ sender: Any) {
+        userDidTapView(self)
+        enableUI(enabled: false)
+        
+        if isValidTextFieldValue(textfield: latitudeTextField, forRange: Constants.Flickr.SearchLatRange) && isValidTextFieldValue(textfield: longitudeTextField, forRange: Constants.Flickr.SearchLongRange) {
+            photoLabel.text = "Searching . . ."
+            var parameters: [String: String?] = getParamPair() as! [String : String]
+            parameters[Constants.FlickrParameterKeys.BoundingBox] = getbbox()
+            displayImageFromFlickrBySearch(parameters: parameters as [String : AnyObject])
+        } else {
+            enableUI(enabled: true)
+            photoLabel.text = "Latitude should be [-90.0, 90.0].\n Longitude should be [-180.0, 180.0]."
+        }
+    }
+    
+    // MARK: Check Validation for lat/long values
+    private func isValidTextFieldValue(textfield: UITextField, forRange: (Double, Double)) -> Bool {
+        
+        if let value = Double(textfield.text!), !textfield.text!.isEmpty {
+            return isValueInRange(value: value, min: forRange.0, max: forRange.1)
+        } else {
+            return false
+        }
+    }
+    
+    private func isValueInRange(value: Double, min: Double, max: Double) -> Bool {
+        return !(value < min || value > max)
+    }
+    
+    // MARK: get bboxString
+    private func getbbox() -> String {
+        // ensure bbox is bounded
+        if let latitude = Double(latitudeTextField.text!), let longitude = Double(longitudeTextField.text!) {
+            let minLong = max(longitude - Constants.Flickr.SearchBoxWidth, Constants.Flickr.SearchLongRange.0)
+            let minLat =  max(latitude - Constants.Flickr.SearchBoxHeight, Constants.Flickr.SearchLatRange.0)
+            let maxLong = min(longitude + Constants.Flickr.SearchBoxWidth, Constants.Flickr.SearchLongRange.1)
+            let maxLat = min(latitude + Constants.Flickr.SearchBoxHeight, Constants.Flickr.SearchLatRange.1)
+            return "\(minLong),\(minLat),\(maxLong),\(maxLat)"
+        } else {
+           return "0,0,0,0"
+        }
     }
 }
 
+//MARK: ViewController: Configure UI
 
 extension ViewController {
+    
+    // enable/disable UIViews
     private func enableUI(enabled: Bool) {
         photoLabel.isEnabled = enabled
         textSearchField.isEnabled = enabled
@@ -89,14 +131,44 @@ extension ViewController {
             locationSearchButton.alpha = 0.5
         }
     }
+    
+    private func displayImageFromFlickrBySearch(parameters: [String:AnyObject]) {
+        print(flickrUrlFromParameters(parameters: parameters))
+        enableUI(enabled: true)
+        photoLabel.text = "DONE!"
+    }
+    
+    //MARK: Create url from parameters
+    private func flickrUrlFromParameters(parameters: [String: AnyObject]) -> URL {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = Constants.Flickr.APIScheme
+        urlComponents.host = Constants.Flickr.APIHost
+        urlComponents.path = Constants.Flickr.APIBaseURL
+        urlComponents.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            urlComponents.queryItems!.append(queryItem)
+        }
+        return urlComponents.url!
+    }
 }
 
+//MARK: ViewController : UITextFieldDelegate
+
 extension ViewController: UITextFieldDelegate {
+    
+    // textField delegate functions
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
 
+    // show/hide keyboard
     @objc func keyboardWillShow(_ notification:Notification) {
         view.frame.origin.y -= getKeyboardHeight(notification)
     }
@@ -105,10 +177,24 @@ extension ViewController: UITextFieldDelegate {
         view.frame.origin.y += getKeyboardHeight(notification)
     }
     
+    // get keyboard height
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
         
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
+    }
+    
+    private func resignIfFirstResponsder(textfield: UITextField) {
+        if (textfield.isFirstResponder) {
+            textfield.resignFirstResponder()
+        }
+    }
+    
+    // user tap search button
+    private func userDidTapView(_ sender: AnyObject) {
+        resignIfFirstResponsder(textfield: textSearchField)
+        resignIfFirstResponsder(textfield: longitudeTextField)
+        resignIfFirstResponsder(textfield: latitudeTextField)
     }
 }
